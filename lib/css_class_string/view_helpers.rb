1e7ef2f0287fdc5041ef095115fd9cfe4cc980b3
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2014-08-17 22:17:53 +0200

    Fix multiple arguments bug

--- lib/css_class_string/view_helpers.rb
+++ lib/css_class_string/view_helpers.rb
@@ -2,8 +2,8 @@ require "css_class_string/helper"
 
 module CssClassString
   module ViewHelpers
-    def class_string(hash)
-      ::CssClassString::Helper.new(hash).to_s
+    def class_string(*args)
+      ::CssClassString::Helper.new(*args).to_s
     end
   end
 end
